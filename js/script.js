88964b71ac41122bb3393c04afcb7e8d65d51c34
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-10-16 18:30:00 +0200

    add page project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const buttonMenu = document.querySelector('#buttonMenu')
 const modalHeader = document.querySelector('#modalHeader')
 const textWritter = document.querySelector('#textWritter')
-
+const phoneButton = document.querySelector('#phoneButton')
+const screen = document.querySelector('#screen')
+const video = document.querySelector('#video')
+const smartphone = document.querySelector('#smartphone')
 
 /* this function open the modal header */
 buttonMenu.addEventListener('click', ()=>{
@@ -14,15 +17,14 @@ buttonMenu.addEventListener('click', ()=>{
     modalHeader.classList.remove("none")
   }
 })
-
+/* this function write automatically */
   new Typewriter(textWritter,{
   })
   .typeString("!")
-  .pauseFor(2500)
   .deleteChars(2)
   .typeString(', Dev')
   .typeString('<span style="color: #00ffa5;"> JAVASCRIPT !</span>')
-  .pauseFor(500)
+  .pauseFor(2000)
   .deleteChars(13)
   .typeString('<span style="color: #9403f4;"> PHP !</span>')
   .pauseFor(500)
@@ -36,6 +38,48 @@ buttonMenu.addEventListener('click', ()=>{
   .typeString('<span style="color: #00ffa5;"> FULL-STACK !</span> ')
   .start();
 
-addEventListener('scroll', (event) => {
-  console.log(window.scrollY);
-});
+  addEventListener('scroll', (event) => {
+    console.log(window.scrollY);
+    
+    if (window.scrollY > 1150) {
+      smartphone.classList.add("none")
+      smartphone.classList.remove("open")
+    }
+    if (window.scrollY < 1150) {
+      smartphone.classList.remove("none")
+      smartphone.classList.add("open")
+    }
+  }); 
+
+phoneButton.addEventListener('click', ()=>{
+  if (screen.classList.contains('none')) {
+    video.classList.add("none")
+    screen.classList.add("open")
+    screen.classList.remove("none")
+  }else{
+    screen.classList.add("none")
+    video.classList.add("open")
+    video.classList.remove("none")
+    
+  }
+})
+
+window.onload = function () {
+  let controller = new ScrollMagic.Controller({
+    globalSceneOptions:{
+      triggerHook:'onLeave'
+    }
+  })
+  let slides = document.querySelectorAll("section.panel")
+  
+  for (let i = 0; i < slides.length; i++) {
+    console.log(slides);
+    new ScrollMagic.Scene({
+      triggerElement: slides[i]
+    })
+    .setPin(slides[i])
+    .addIndicators()
+    .addTo(controller)
+  }
+}
+
